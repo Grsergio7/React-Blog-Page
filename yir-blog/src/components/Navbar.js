1f78910a486a0462e0f394a583ef5c4c091cde9b
grsergio7 <ruiz.sergio07@gmail.com>
2020-09-18 11:07:37 -0700

    deleted files that were un needed. Started project

---
+++ yir-blog/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                    
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    )
+}
